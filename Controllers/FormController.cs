017b7afcc1cbf14c7e905778479b724e9b03fcb4
İlker Atabay <atabayilker@gmail.com>
2019-03-22 13:44:12 +0300

    Before the class, 22.03.19

---
+++ Controllers/FormController.cs
@@ -0,0 +1,24 @@
+﻿using SimpleBlog.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace SimpleBlog.Controllers
+{
+    public class FormController : Controller
+    {
+        // GET: Register
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Register(FormRegister data)
+        {
+            return View();
+        }
+    }
+}
